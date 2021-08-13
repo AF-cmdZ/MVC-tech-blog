6cfcfc9b9ad04e4d8468fbef55130f3e5f5dfdcc
A Fish <ashley.paz.fish@gmail.com>
2021-08-13 14:28:27 -0500

    user delete

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -143,3 +143,25 @@ router.put('/:id', withAuth, (req, res) => {
     });
 })
 
+// Delete an existing user
+router.delete('/:id', withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this ID'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
+
