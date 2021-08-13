de3cc1e476eb7202f81ba71dcbe435f05c60bfd9
A Fish <ashley.paz.fish@gmail.com>
2021-08-13 13:52:39 -0500

    setup user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+const session = require('express-session');
+const withAuth = require('../../utils/auth');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// GET all users
+router.get('/', (req, res) => {
+    // access the User model
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
