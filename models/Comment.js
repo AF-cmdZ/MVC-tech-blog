d01acebcb62d5b97e4e7813c121890da896f5bf5
A Fish <ashley.paz.fish@gmail.com>
2021-08-27 11:42:41 -0500

    start Comment model

---
+++ models/Comment.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // character must be at least 5 characters long
+                len: [5]
+            }
+        }
+    },
+    {
+        // hooks for password hashing
+        hooks: {
+            async beforeCreate(newUserData) {
+                // adds 10 digits of 'salt'
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
