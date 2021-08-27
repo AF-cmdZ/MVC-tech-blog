f87f521383af5898e3b746c38e87458e916adcab
A Fish <ashley.paz.fish@gmail.com>
2021-08-27 12:40:18 -0500

    start seeds/index.js

---
+++ seeds/index.js
@@ -0,0 +1,5 @@
+const seedUsers = require('./user-seeds.js');
+const seedPosts = require('./post-seeds.js');
+const seedComments = require('./comment-seeds.js');
+
+const sequelize = require('../config/connection.js');
