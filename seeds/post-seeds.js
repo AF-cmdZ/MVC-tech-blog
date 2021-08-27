d16f05ae32927c838196be3e8ba02369e92f68d0
A Fish <ashley.paz.fish@gmail.com>
2021-08-27 12:14:50 -0500

    start post seeds

---
+++ seeds/post-seeds.js
@@ -0,0 +1,34 @@
+const { Post } = require('../models');
+
+const postData = [
+    {
+        title:
+        post_text:
+        user_id:
+    },
+    {
+        title:
+        post_text:
+        user_id:
+    },
+    {
+        title:
+        post_text:
+        user_id:
+    },
+    {
+        title:
+        post_text:
+        user_id:
+    },
+    {
+        title:
+        post_text:
+        user_id:
+    },
+    {
+        title:
+        post_text:
+        user_id:
+    }
+]
