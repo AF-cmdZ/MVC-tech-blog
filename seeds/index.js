3eb8b198f270b8dd31f60a3287c003a11f849692
A Fish <ashley.paz.fish@gmail.com>
2021-08-27 12:42:30 -0500

    finish seed initialize

--- seeds/index.js
+++ seeds/index.js
@@ -3,3 +3,14 @@ const seedPosts = require('./post-seeds.js');
 const seedComments = require('./comment-seeds.js');
 
 const sequelize = require('../config/connection.js');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+    
+    process.exit(0);
+};
+
+seedAll();
