051a6cd998f1f9856f830447ec534b7f7e07747f
A Fish <ashley.paz.fish@gmail.com>
2021-08-29 12:47:20 -0500

    create a new post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -74,3 +74,17 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// Create a new post
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_text: req.body.post_text,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
