c2105bb054badda24fc6011e8b757f05f4770bee
A Fish <ashley.paz.fish@gmail.com>
2021-08-29 15:15:33 -0500

    sign up

---
+++ public/js/signup.js
@@ -0,0 +1,31 @@
+document
+.querySelector('.signup-form');
+
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+            if (response.ok) {
+                alert('Account Created!');
+                document.location.replace('/dashboard');
+            } else {
+                alert(response.statusText)
+            }
+        }
+};
+
+document.addEventListener('submit', signupFormHandler);
