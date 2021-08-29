56d999b4e3859c170445ca503b2880b95bb4ce31
A Fish <ashley.paz.fish@gmail.com>
2021-08-29 15:10:47 -0500

    logout

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+document
+.querySelector('#logout');
+
+
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'}
+    });
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.addEventListener('submit', logout);
