19be374013861d7db3df43334be2b3b71d09cd03
A Fish <ashley.paz.fish@gmail.com>
2021-08-29 15:28:53 -0500

    delete post js

---
+++ public/js/delete-post.js
@@ -0,0 +1,22 @@
+document
+.querySelector('.delete-post-btn');
+
+
+const deleteFormHandler = async (event) => {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/') [
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        }),
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    };
+
+document.addEventListener('submit', deleteFormHandler);
