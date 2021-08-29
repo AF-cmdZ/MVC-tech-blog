00d8e278e077b8794a251caec631248db6a3f8b0
A Fish <ashley.paz.fish@gmail.com>
2021-08-29 16:43:36 -0500

    remove duplicate withAuth

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
-const withAuth = require('../../utils/auth');
 // const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
